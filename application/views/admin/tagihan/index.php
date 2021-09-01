ee19dfea595e2dd8e5aec1cab59429e1a156cc20
Aldiskax29 <muhamadaldisetiawan02@gmail.com>
2021-09-01 16:24:17 +0700

    nambahkan nomor

--- application/views/admin/tagihan/index.php
+++ application/views/admin/tagihan/index.php
@@ -27,7 +27,7 @@
                           <table class="table table-striped" id="myTable">
                               <thead>
                                   <tr>
-                                      <th>No</th>
+                                      <th>Nomor</th>
                                       <th>Jenis Tagihan</th>
                                       <th>Grade</th>
                                       <th>Tahun Ajaran</th>
@@ -45,7 +45,7 @@
                                           <td>Rp. <?= number_format($item->jumlah_tagihan, 0, ',', '.') ?></td>
                                           <td>
                                               <button class="btn btn-danger btn-sm" onclick="return confirm('Yakin Mau Di Hapus') ? window.location.href = '<?= site_url('admin/tagihan/delete/' . $item->id) ?>' : ''"><i class="fas fa-trash"></i></button>
-                                              <a href="" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i></a>
+                                              <!-- <a href="" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i></a> -->
                                           </td>
                                       </tr>
                                   <?php endforeach ?>
